76f2cfab6a3f400e2f77eaaa057655cc91b5ad8f
lyz <408772145@qq.com>
2016-11-01 13:05:43 +0800

    Finished wxAuth

--- src/module/wxAuth/app.vue
+++ src/module/wxAuth/app.vue
@@ -5,47 +5,43 @@
 <script>
 import Lib from 'assets/Lib.js'
 export default {
-  data() {
-    return {
-      lastUrl: Lib.M.GetRequest().url?unescape(Lib.M.GetRequest().url):'./index.html',
-      code: Lib.M.GetRequest().code,
-      appId: "wx532853a12ecebf6b"
-    }
-  },
-  ready() {
-    if (Lib.M.isAuth()) {
-      location.href = this.lastUrl
-    } else {
-      this.getCode()
-    }
-  },
-  methods: {
-    getCode() {
-      if (this.code) {
-        this.$http.post(`${Lib.C.userApi}auth/loginUsingWechatQy`, {
-          code: this.code
-        }, {
-          xhr: {
-            withCredentials: true
-          },
-          emulateJSON: true
-        }).then((res) => {
-          window.localStorage.setItem("user",JSON.stringify(res.data.data))
-          if(res.data.data.profile.mobile){
+    data() {
+        return {
+            lastUrl: Lib.M.GetRequest().url ? unescape(Lib.M.GetRequest().url) : './index.html',
+            code: Lib.M.GetRequest().code,
+            appId: "wx532853a12ecebf6b"
+        }
+    },
+    ready() {
+        if (Lib.M.isAuth()) {
             location.href = this.lastUrl
-          }else{
-            location.href = `./verifyPhone.html?url=${encodeURIComponent(this.lastUrl)}`
-          }
-        }, (res) => {
-          alert("微信登录失败，请稍后重试")
-          console.log(res)
-          return false;
-        })
-      } else {
-        let a = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${this.appId}&redirect_uri=${location.href}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect`
-        location.href = a
-      }
+        } else {
+            this.getCode()
+        }
+    },
+    methods: {
+        getCode() {
+            if (this.code) {
+                this.$http.post(`${Lib.C.userApi}auth/loginUsingWechatQy`, {
+                    code: this.code
+                }, {
+                    xhr: {
+                        withCredentials: true
+                    },
+                    emulateJSON: true
+                }).then((res) => {
+                    window.localStorage.setItem("user", JSON.stringify(res.data.data))
+                    location.href = this.lastUrl
+                }, (res) => {
+                    alert("微信登录失败，请稍后重试")
+                    console.log(res)
+                    return false;
+                })
+            } else {
+                let a = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${this.appId}&redirect_uri=${location.href}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect`
+                location.href = a
+            }
+        }
     }
-  }
 }
 </script>
